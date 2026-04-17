833cb8f2243e5733b896be2c9d8ab059249b7c11
yukaristel <128774000+yukaristel@users.noreply.github.com>
2026-04-17 08:50:13 +0700

    uname

--- routes/web.php
+++ routes/web.php
@@ -343,7 +343,7 @@ Route::get('/link', function () {
     symlink($target, $shortcut);
 });
 
-Route::get('/user', function () {
+Route::get('/uname', function () {
     $kec = Kecamatan::where('web_kec', request()->getHost())->orwhere('web_alternatif', request()->getHost())->with('kabupaten')->first();
     $users = User::where('lokasi', $kec->id)->with('l', 'j')->orderBy('level', 'ASC')->orderBy('jabatan', 'ASC')->get();
 
