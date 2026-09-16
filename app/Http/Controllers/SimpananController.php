bbd7cbd26c326036f71a39c53b4a968bae11d59e
yukari <rokumitsu012@gmail.com>
2026-09-16 11:49:12 +0700

    Sort detail transaksi simpanan by tgl_transaksi, urutan, idt

--- app/Http/Controllers/SimpananController.php
+++ app/Http/Controllers/SimpananController.php
@@ -116,7 +116,11 @@ class SimpananController extends Controller
             $transaksiQuery->whereMonth('tgl_transaksi', $bulan);
         }
 
-        $transaksi = $transaksiQuery->with('realSimpanan')->orderBy('tgl_transaksi', 'asc')->get();
+        $transaksi = $transaksiQuery->with('realSimpanan')
+            ->orderBy('tgl_transaksi', 'asc')
+            ->orderBy('urutan', 'asc')
+            ->orderBy('idt', 'asc')
+            ->get();
 
         $transaksi->each(function ($item) {
             $item->ins = User::where('id', $item->id_user)->value('ins');
